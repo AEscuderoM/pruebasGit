ee6746c6619a2e71221336a7d70cd3cb3e4b15dc
antonio <antonio_escudero2002@hotmail.com>
2015-03-21 00:22:56 +0100

    Y encima te toco el hello.c con visual studio 2010

--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
+//jose no has aprendido nada, no hay ni un comentario
 int main(void) {
   printf("Hola mundo\n");
-  return 0;
+  printf("Hay una leyenda que dice que si tu primer programa no es un hola mundo morirás en 3 días");
+return 0;
 }
